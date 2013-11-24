baeb8562cf956375dbafce02d386e84c1ee06d80
Martijn van Steenbergen <martijn@van.steenbergen.nl>
2013-11-24 15:25:40 +0100

    JsonGrammar example in terms of first version of HypeScript

---
+++ Example.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Example where
+
+import HypeScript
+
+import Prelude hiding (id, (.))
+import Control.Category (Category(..))
+import Data.Aeson (Value)
+import Data.Text (Text)
+
+data Person = Person
+  { name   :: Text
+  , gender :: Gender
+  , age    :: Int
+  , lat    :: Float
+  , lng    :: Float
+  }
+
+data Gender = Male | Female
+
+cPerson :: Grammar c
+  (Text :- Gender :- Int :- Float :- Float :- t)
+  (Person :- t)
+cPerson = undefined
+
+gGender :: Grammar Val (Value :- t) (Gender :- t)
+gGender = undefined
+
+cMale, cFemale :: Grammar c (Gender :- t) t
+cMale = undefined
+cFemale = undefined
+
+gPerson :: Grammar Val (Value :- t) (Person :- t)
+gPerson = cPerson . Object
+  ( Property "name" gText 
+  . Property "gender" gGender 
+  . Property "age" gInt
+  . Property "lat" gFloat
+  . Property "lng" gFloat
+  )
---
+++ HypeScript.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE GADTs #-}
+
+module HypeScript where
+
+import Prelude hiding (id, (.))
+import Control.Category (Category(..))
+import Data.Aeson (Value)
+import Data.Text (Text)
+
+data h :- t = h :- t
+infixr 5 :-
+
+data Context = Val | Obj | Arr
+
+data Grammar (c :: Context) t1 t2 where
+  -- Any context
+  Id :: Grammar c t t
+  (:.) :: Grammar c t2 t3 -> Grammar c t1 t2 -> Grammar c t1 t3
+
+  -- Value context
+  Literal :: Value -> Grammar Val (Value :- t) t
+
+  -- Object context
+  Object :: Grammar Obj t1 t2 -> Grammar Val (Value :- t1) t2
+  Property :: Text -> Grammar Val (Value :- t) (a :- t) -> Grammar Obj t (a :- t)
+
+  -- Array context
+  Array :: Grammar Arr t1 t2 -> Grammar Val (Value :- t1) t2
+  Element :: Grammar Val t (a :- t) -> Grammar Arr t (a :- t)
+
+instance Category (Grammar c) where
+  id = Id
+  (.) = (:.)
+
+gText :: Grammar Val (Value :- t) (Text :- t)
+gText = undefined
+
+gInt :: Grammar Val (Value :- t) (Int :- t)
+gInt = undefined
+
+gFloat :: Grammar Val (Value :- t) (Float :- t)
+gFloat = undefined
