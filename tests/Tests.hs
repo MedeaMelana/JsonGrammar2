2af0d50b0e5f109507c7436e0946e9d7beb265fc
Martijn van Steenbergen <martijn@van.steenbergen.nl>
2014-06-01 13:19:24 +0200

    More structure in Tests.hs

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -4,9 +4,8 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TemplateHaskell #-}
 
-import Types
-
 import Language.JsonGrammar
+import Types
 
 import Prelude hiding (id, (.))
 import Control.Category (Category(..))
@@ -17,14 +16,19 @@ import Data.StackPrism (StackPrism)
 import Data.StackPrism.TH (deriveStackPrismsWith)
 import Data.Text (Text)
 import Language.TypeScript (renderDeclarationSourceFile)
-
 import Test.Framework (Test, defaultMain)
 import Test.Framework.Providers.HUnit (testCase)
 import Test.HUnit (assertBool)
 
+
+-- Create stack prisms for our three datatypes (defined in module Types)
+
 deriveStackPrismsWith (map toLower) ''Person
 deriveStackPrismsWith (map toLower) ''Coords
 deriveStackPrismsWith (map toLower) ''Gender
+
+
+-- Write Json instances for the datatypes
 
 instance Json Gender where
   grammar = fromPrism male   . literal "male"
@@ -37,13 +41,11 @@ instance Json Person where
     . prop "gender"
     . (prop "age" <> defaultValue 42)
     . fromPrism coords
-    -- . prop "lat"
-    -- . prop "lng"
     . property "coords" (array (el . el))
     )
 
-personGrammar :: Grammar Val (Value :- t) (Person :- t)
-personGrammar = grammar
+
+-- Create two example values
 
 alice :: Person
 alice = Person "Alice" Female 21 (Coords 52 5)
@@ -51,33 +53,29 @@ alice = Person "Alice" Female 21 (Coords 52 5)
 bob :: Person
 bob = Person "Bob" Male 22 (Coords 53 6)
 
+
+-- Two tests: one for lists, the other for tuples
+
+test1, test2 :: Test
+test1 = testCase "PersonList"  $ assertBool "" (checkInverse [alice, bob])
+test2 = testCase "PersonTuple" $ assertBool "" (checkInverse (alice, bob))
+
 checkInverse :: (Json a, Eq a) => a -> Bool
 checkInverse value = value == value'
   where
-    Just (json :- ()) = unparseValue grammar (value :- ())
+    Just (json   :- ()) = unparseValue grammar (value :- ())
     Just (value' :- ()) = parseMaybe (parseValue grammar) (json :- ())
 
-test1 :: Test
-test1 = testCase "PersonList" (assertBool "" (checkInverse [alice, bob]))
 
-test2 :: Test
-test2 = testCase "PersonTuple" (assertBool "" (checkInverse (alice, bob)))
-
-printInterfaces :: [SomeGrammar Val] -> IO ()
-printInterfaces gs = putStrLn (renderDeclarationSourceFile (interfaces gs))
-
-consList :: (forall t'. Grammar Val (Value :- t') (a :- t')) ->
-            Grammar Val (Value :- t) ([a] :- t)
-consList g = label "Node" (nilCase <> consCase)
-  where
-    nilCase = nil . literal Null
-    consCase =
-      cons . object
-        ( property "head" g
-        . property "tail" (consList g)
-        )
+-- Write the TypeScript definition to stdout and run the tests
 
 main :: IO ()
 main = do
   printInterfaces [SomeGrammar personGrammar]
   defaultMain [test1, test2]
+
+personGrammar :: Grammar Val (Value :- t) (Person :- t)
+personGrammar = grammar
+
+printInterfaces :: [SomeGrammar Val] -> IO ()
+printInterfaces gs = putStrLn (renderDeclarationSourceFile (interfaces gs))
